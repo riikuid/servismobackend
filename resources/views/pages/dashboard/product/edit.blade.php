da71442a6244d51854ffe3416d5ce8ebb8c74418
riikuid <fahmibambe@gmail.com>
2023-05-15 13:37:19 +0700

    edit description

--- resources/views/pages/dashboard/product/edit.blade.php
+++ resources/views/pages/dashboard/product/edit.blade.php
@@ -101,9 +101,7 @@
                                 </div>
                                 <div class="px-4 py-2 bg-white rounded-b-lg dark:bg-gray-800">
                                     <label for="description" class="sr-only">Description</label>
-                                    <textarea id="description" name="description" rows="8" class="block w-full px-0 text-sm text-gray-800 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400" placeholder="Write an description..." required>
-                                        {{ old('description') ?? $item->description }}
-                                    </textarea>
+                                    <textarea id="description" name="description" rows="8" class="block w-full px-0 text-sm text-gray-800 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400" placeholder="Write an description..." required>{{ old('description') ?? $item->description }}</textarea>
                                 </div>
                             </div>
                         </div>
